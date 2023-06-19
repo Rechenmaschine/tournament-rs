91c0072abec2721539105d0f471dfa3ada35cbaa
Louis Schell <louis.schell1@gmx.de>
2023-06-19 22:50:23 +0200

    Add prelude module to lib.rs for easier access

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,12 @@ pub mod ranking;
 pub mod scheduling;
 pub mod scoring;
 pub mod tournaments;
+
+pub mod prelude {
+    pub use crate::game::{Match, MatchResult};
+    pub use crate::player::{Player, PlayerId, PlayerRepository};
+    pub use crate::ranking::RankingPolicy;
+    pub use crate::scheduling::Scheduler;
+    pub use crate::scoring::ScoringSystem;
+    pub use crate::tournaments::Tournament;
+}
